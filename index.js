d5e64b6d4cbabe3f73e47a3b80357c479f4dd91d
João Carneiro <153639757+JoaoCarneiroo@users.noreply.github.com>
2024-05-10 00:40:57 +0100

    Adicionar Token quando se faz login ou registo

--- index.js
+++ index.js
@@ -2,11 +2,29 @@ const express = require('express');
 const collection = require('./mongo');
 const cors = require('cors');
 const bcrypt = require('bcryptjs'); 
+const jwt = require('jsonwebtoken');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+const JWT_SECRET = 'sua_chave_secreta_aqui';
+
+
+// Função para verificar o token
+function verifyToken(req, res, next) {
+  const token = req.headers['authorization'];
+  if (!token) return res.status(403).json({ auth: false, message: 'Token não fornecido.' });
+
+  jwt.verify(token, process.env.JWT_SECRET, function(err, decoded) {
+    if (err) return res.status(500).json({ auth: false, message: 'Falha ao autenticar o token.' });
+
+    // Se tudo estiver correto, salva no request para uso posterior
+    req.userId = decoded.id;
+    next();
+  });
+}
 
 app.get('/', cors(), (req, res) => {});
 
@@ -19,7 +37,14 @@ app.post('/login', async (req, res) => {
     if (check) {
       const matchPassword = await bcrypt.compare(password, check.password);
       if (matchPassword){
-        res.json('Sucesso');
+        // Criação do token JWT
+        const token = jwt.sign({ id: check._id }, JWT_SECRET, {
+          expiresIn: 86400 // expira em 24 horas
+        });
+        res.header('authorization', token)
+        res.json({token, userId:check._id, message:'Sucesso'});
+
+
       } else {
         res.json('PasswordErrada')
       }
@@ -27,8 +52,8 @@ app.post('/login', async (req, res) => {
       res.json('UtilizadorNaoExiste');
     }
   } catch (e){
-  res.status(500).json('Erro');
-  console.log(e);
+      res.status(500).json('Erro');
+      console.log(e);
   }
 });
 
@@ -50,12 +75,22 @@ app.post('/register', async (req, res) => {
       };
     
       await collection.insertMany([data]);
-      res.json('Sucesso');
+      // Criação do token JWT
+      const token = jwt.sign({ id: data._id }, JWT_SECRET, {
+        expiresIn: 86400 // expira em 24 horas
+      });
+      res.header('authorization', token)
+      res.json({token, userId:data._id, message:'Sucesso'});
     }
   } catch (e) {
     res.status(500).json('Erro');
     console.log(e);
   }
+});
+
+// Rota protegida
+app.get('/profile', verifyToken, (req, res) => {
+  res.json('Conteúdo protegido');
 });
 
 app.listen(3000, () => {
